bd807fb625b225cc2dc686a72c2d788aa4200ab5
Jefferson A. Silva <jeff2248@hotmail.com>
2021-01-06 15:33:57 -0300

    projeto finalizado

---
+++ src/data.js
@@ -0,0 +1,40 @@
+const people = [
+  {
+    id: 1,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg",
+    name: "maria ferguson",
+    title: "Front End",
+    quote:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse leo erat, accumsan id tempor sed, lacinia non orci.",
+  },
+  {
+    id: 2,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+    name: "Paulo Fonseca",
+    title: "Testes automáticos",
+    quote:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse leo erat, accumsan id tempor sed, lacinia non orci.",
+  },
+  {
+    id: 3,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959121/person-1_aufeoq.jpg",
+    name: "Lucas Santos",
+    title: "Designer",
+    quote:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse leo erat, accumsan id tempor sed, lacinia non orci.",
+  },
+  {
+    id: 4,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+    name: "Andressa Oliveira",
+    title: "Chefe",
+    quote:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse leo erat, accumsan id tempor sed, lacinia non orci.",
+  },
+];
+
+export default people;
